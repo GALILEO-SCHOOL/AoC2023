0169f8a5b3581fea3c4c904a09d04c9871e92a5f
Palmanius <hrdy@gsedu.sk>
2023-12-05 12:01:57 +0100

    A

--- Hrdy/Day5/Day5.py
+++ Hrdy/Day5/Day5.py
@@ -10,7 +10,8 @@ a = []
 with open("Hrdy\Day5\Test.txt","r") as f:
     Seeds = [int(x) for x in f.readline().split(": ")[1].strip().split(" ")]
     for line in f:
-        a.append(line.strip())
+        if line.strip() != "":
+            a.append(line.strip())
 
 print(Seeds)
 print(a)
