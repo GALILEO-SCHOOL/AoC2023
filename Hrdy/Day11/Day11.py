2a16507b430dce1645737ff1a23e2c5d2e150599
Palmanius <hrdy@gsedu.sk>
2023-12-21 12:22:05 +0100

    Update Day11.py

--- Hrdy/Day11/Day11.py
+++ Hrdy/Day11/Day11.py
@@ -1,4 +1,4 @@
-with open("Hrdy\Day11\Day11.txt","r") as f:
+with open("Hrdy\Day11\Test.txt","r") as f:
     Universe = []
     Galaxies = []
     AmountOfGalaxies = 1
@@ -10,30 +10,46 @@ with open("Hrdy\Day11\Day11.txt","r") as f:
         for c in line.strip():
             if c == "#":
                 Gala = True
-                Row.append([AmountOfGalaxies,y,x])
-                Galaxies.append([AmountOfGalaxies,y,x])
+                Row.append([AmountOfGalaxies,y,x,0,0])
+                Galaxies.append([AmountOfGalaxies,y,x,0,0])
                 AmountOfGalaxies +=1
             else: Row.append(c)
-            x += 1
+            x+=1
+        y+=1
         Universe.append(Row)
-        if not Gala: y+=2
-        else: y+=1
+
+
+ExpansionRate = 1
 
 Horizont = [x[2] for x in Galaxies]
+VerticalLimit = [x[1] for x in Galaxies]
+
 Expansion = 0
 for x in range(max(Horizont)):
     if x not in Horizont:
         for index,Gala in enumerate(Galaxies):
             if Gala[2] > x+Expansion:
-                Galaxies[index][2] += 1
+                Galaxies[index][4] += ExpansionRate
+        Expansion += 1
+
+Expansion = 0
+for y in range(max(VerticalLimit)):
+    if y not in VerticalLimit:
+        for index,Gala in enumerate(Galaxies):
+            if Gala[1] > y+Expansion:
+                Galaxies[index][3] += ExpansionRate
         Expansion += 1
 
 # for line in Universe:
-#     print(line)
-# print(Galaxies)
+#      print(line)
+
 
 Distance = 0
+for i,Gal in enumerate(Galaxies):
+    Galaxies[i][1] += Galaxies[i][3]
+    Galaxies[i][2] += Galaxies[i][4]
+print(Galaxies)
 for i in range(len(Galaxies)-1):
     for j in range(i+1,len(Galaxies)):
-        Distance += (abs(Galaxies[i][1] - Galaxies[j][1]) + abs(Galaxies[i][2] - Galaxies[j][2]))
+        Distance += abs((Galaxies[i][1]) - (Galaxies[j][1])) + abs((Galaxies[i][2]) - (Galaxies[j][2]))
 print(Distance)
